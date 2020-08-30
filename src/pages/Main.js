88d55e90de15bfe58e10534df64df254427e6c26
Michael Lopez <mlopez@rightslne.com>
2020-08-29 22:38:24 -0700

    improved calc UI and function

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,66 +1,144 @@
 import React from 'react';
+import './App.css'
 
 const Main = () => {
 
-    const [display, setDisplay] = React.useState(0);
-    const [input1, setInput1] = React.useState(0);
+    const [display, setDisplay] = React.useState("");
+    const [input1, setInput1] = React.useState("");
+    const [operator, setOperator] = React.useState(""); // used this to store the value to store key word
 
    const handleClickNumbers = (num) => {
-       let result = parseInt(display.toString() + num.toString()); 
-       setDisplay(result); 
+
+   
+       
+       if(operator) {
+        let result = parseInt(input1.toString() + num.toString()); 
+           setInput1(result); 
+       } else {
+        let result = parseInt(display.toString() + num.toString());  
+        setDisplay(result); 
+       }
+       
    }; 
 
    const clear = () =>{
-       setDisplay(0); 
-       setInput1(0);
+       setDisplay(""); 
+       setInput1("");
+       setOperator(""); 
    }
 
    
    const add = () =>{
-       //save first input
-        setInput1(display);
-        setDisplay(0); 
+     
+        setOperator("+"); 
    }
 
+   const divide = () => {
+
+       setOperator("/") 
+   }
+
+   const multiply = () => {
+
+       setOperator("*"); 
+   }
+
+   const subtract = () => {
+
+        setOperator("-"); 
+   }
    const equal = () => {
-       setDisplay(display + input1); 
-       setInput1(0); 
+       
+       if (operator == "+") {
+        setDisplay(display + input1);
+    
+        }
+
+       if (operator == "-") {
+           setDisplay((display - input1) * -1); 
+           
+       } 
+
+       if (operator == "/") {
+           setDisplay(display / input1);
+       }
+
+       if (operator == "*") {
+           setDisplay(display * input1);
+       }
+
+       setInput1(""); 
+       setOperator(""); 
+
    }
 
     return ( 
     <>
-    <span style={{fontSize: 50}}>{display}</span> 
-    <div>
-    <span style={{fontSize: 20, color: 'green'}}>{input1}</span>
+
+    
+
+<div className="app">
+    <div className="input">   
+        <div>
+            <span style={{fontSize: 50, color: 'white'}}>{display}</span>
+        </div>
+
+        <div>
+         <span style={{fontSize: 50, color: 'white'}}>{operator}</span>
+        </div>
+        
+        <div>
+            <span style={{fontSize: 50, color: 'white'}}>{input1}</span> 
+        </div>
+     
     </div>
+    <div className="calc-wrapper">
+        <div>
+            <div className="row">
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(1)}>1</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(2)}>2</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(3)}>3</button>
+                <button className="operator" onClick = {() => add()}>+</button>
+
+            </div>
+            <div className="row">
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(4)}>4</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(5)}>5</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(6)}>6</button>
+                <button className="operator" onClick = {() => subtract()}>-</button>
+
+            </div>
+            <div className="row">
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(7)}>7</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(8)}>8</button>
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(9)}>9</button>
+                <button className="operator" onClick = {() => divide()}>/</button>
+            </div>
+            <div className="row">
+                <button className="button-wrapper" onClick = {() => handleClickNumbers(0)}>0</button>
+                <button className="operator" onClick = {() => multiply()}>*</button>
+    
+            </div>
+            <div className="row">
+                <button className="button-wrapper" onClick = {() => clear()}>CLEAR</button>
+                <button className="operator" onClick = {() => equal()}>=</button>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
      
     
-    <div>
-     <button onClick = {() => handleClickNumbers(1)}>1</button>
-     <button onClick = {() => handleClickNumbers(2)}>2</button>
-     <button onClick = {() => handleClickNumbers(3)}>3</button>
-    </div>
-   <div>
-    <button onClick = {() => handleClickNumbers(4)}>4</button>
-    <button onClick = {() => handleClickNumbers(5)}>5</button>
-    <button onClick = {() => handleClickNumbers(6)}>6</button>
-   </div>
     
-    <div>
-    <button onClick = {() => handleClickNumbers(7)}>7</button>
-    <button onClick = {() => handleClickNumbers(8)}>8</button>
-    <button onClick = {() => handleClickNumbers(9)}>9</button>
-    </div>
-    
-    <div>
-    <button onClick = {() => handleClickNumbers(0)}>0</button>
-    <button onClick = {() => add()}>+</button>
-    <button onClick = {() => equal()}>=</button>
-    </div>
 
-    <div>
-        <button onClick = {() => clear()}>CLEAR</button>
-    </div>
+    
+
+
+
+
+
     
     </>
     )
